c8455d20149e951db44d0666ba5de0666d338fa5
sunnyxyst <sunnyxyst@gmail.com>
2026-06-25 23:31:53 +0900

    day03 Hover Lift Card

--- .history/day03/src/main_20260625225010.js
+++ .history/day03/src/main_20260625225010.js
@@ -9,35 +9,34 @@ document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.PlaneGeometry(1, 1.5);
 const material = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    side: THREE.DoubleSide
-  });
+  color: 0xff0000,
+  side: THREE.DoubleSide,
+})
 const card = new THREE.Mesh(geometry, material);
 scene.add(card);
 
-const mouse = {
-  x : 0,
-  y : 0
+const mouse = {  
+  x: 0,
+  y: 0
 }
+
 window.addEventListener('mousemove', (e) => {
   mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
-
-  card.rotation.x += (mouse.y - card.rotation.x) * 0.08;
-  card.rotation.y += (mouse.x - card.rotation.y) * 0.01;
-  // lerp 
-  // current += (target - current) * speed
+  // lerp: current += (target / current) * speed
+  // 마우스가 움직일 때 카드가 회전하는 이벤트를 생성 -> 카드 y축 회전 값 설정, 카드 x축 회전값 설정
+  card.rotation.y += (mouse.x / card.rotation.y) * 0.04;
+  card.rotation.x += (mouse.y / card.rotation.x) * 0.08;
+  
 })
 
 let time = 0;
 function animate() {
   requestAnimationFrame(animate);
 
-  time += 0.01;
-  card.position.y = Math.sin(time) * 0.01;
-  card.rotation.y += 0.005;
+  time += 0.005;
+  card.position.y = Math.sin(time) * 0.04;
+  card.rotation = 
   renderer.render(scene, camera);
-
 }
-
 animate();
