68de938e4f77c12de200db7d8df6061410793b77
sunnyxyst <sunnyxyst@gmail.com>
2026-06-27 16:11:33 +0900

    Day05 Interactive Card

---
+++ .history/day05/src/main_20260627160331.js
@@ -0,0 +1,71 @@
+// input -> state -> target -> lerp -> render
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100);
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.PlaneGeometry(1, 1.5);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  side: THREE.DoubleSide
+});
+const card = new THREE.Mesh(geometry, material);
+scene.add(card);
+camera.position.z = 3;
+
+const mouse = {
+  x: 0,
+  y: 0
+}
+
+// state 
+let isPressed = false;
+let isHovered = false;
+
+// move input
+window.addEventListener('mousemove', (e) => {
+  mouse.x = (e.clientX - window.innerWidth) * 2 - 1;
+  mouse.y = -(e.clientY - window.innerHeight) * 2 + 1;
+})
+
+// hover input
+renderer.domElement.addEventListener('mouseenter', () => {
+  isHovered = true;
+})
+renderer.domElement.addEventListener('mouseout', () => {
+  isHovered = false;
+})
+
+// press input
+renderer.domElement.addEventListener('mousedown', () => {
+  isPressed = true;
+})
+renderer.domElement.addEventListener('mouseup', () => {
+  isPress = false;
+});
+
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  //lerp 
+  let targetZ = 0;
+  let targetRotation = 0;
+  let targetScale = 1;
+  card.rotation.y = (targetRotation - card.rotation.y) * 0.08;
+
+  if(isHovered) {
+    targetScale = 1.3;
+  }
+  card.scale.x += (targetScale - card.scale.x) * 0.08;
+
+  if(isPressed) {
+
+  }
+  renderer.render(scene, camera);
+}
+animate();
