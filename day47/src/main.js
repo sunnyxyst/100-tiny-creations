1a8a4e39ad290e06fea6f7ba57246ca9441962f8
sunnyxyst <hyunnyta@hotmail.com>
2026-08-08 22:21:47 +0900

    day47 - Cross Drift + Counter Tilt

---
+++ day47/src/main.js
@@ -0,0 +1,84 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 3;
+const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.PlaneGeometry(4,2);
+const material = new THREE.MeshStandardMaterial({
+  color: 0x222222,
+  // emissive: 0xffffff,
+  // emissiveIntensity: 0.3,
+  roughness: 0.3,
+  side: THREE.DoubleSide
+});
+const card = new THREE.Mesh(geometry, material);
+scene.add(card);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.z = 1;
+scene.add(ambientLight);
+card.add(pointLight);
+
+const mouse = {
+  x: 999,
+  y: 999
+}
+window.addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+});
+
+const raycaster = new THREE.Raycaster();
+const localPoint = new THREE.Vector3();
+
+let isHovered = false;
+
+function animate() {
+  requestAnimationFrame(animate);
+  raycaster.setFromCamera(mouse, camera);
+
+  const intersects = raycaster.intersectObject(card);
+  isHovered = intersects.length > 0;
+
+  let targetX = 0;
+  let targetY = 0;
+  let targetRotationX = 0;
+  let targetRotationY = 0;
+  let targetLightX = 0;
+  let targetLightY = 0;
+  let targetLightIntensity = 0;
+  
+  if(isHovered) {
+    localPoint.copy(intersects[0].point);
+    card.worldToLocal(localPoint);
+
+    const normalizedX = Math.abs(localPoint.x)/ 2;
+    const normalizedY = Math.abs(localPoint.y) / 1;
+
+    targetX = localPoint.y * normalizedY * 0.15;
+    targetY = -localPoint.x * normalizedX * 0.15;
+    targetRotationX = -localPoint.y * normalizedY * 0.3;
+    targetRotationY = localPoint.x * normalizedX * 0.3;
+    targetLightX = localPoint.x;
+    targetLightY = localPoint.y;
+    targetLightIntensity = 3;
+  }
+
+  card.position.x += (targetX - card.position.x) * 0.05;
+  card.position.y += (targetY - card.position.y) * 0.05;
+  card.rotation.x += (targetRotationX - card.rotation.x) * 0.06;
+  card.rotation.y += (targetRotationY - card.rotation.y) * 0.06;
+  
+  pointLight.position.x += (targetLightX -pointLight.position.x) * 0.05;
+  pointLight.position.y += (targetLightY -pointLight.position.y) * 0.05;
+  pointLight.intensity += (targetLightIntensity -pointLight.intensity) * 0.06;
+
+  renderer.render(scene, camera);
+}
+
+animate();
